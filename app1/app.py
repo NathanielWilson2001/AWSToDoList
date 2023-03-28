316495ed92415d00c4cc9e39e6bd43539e570807
Nathaniel Wilson <nt680476@dal.ca>
2023-03-28 13:52:12 -0300

    Made the add task lambda function

--- app1/app.py
+++ app1/app.py
@@ -4,9 +4,9 @@ import hashlib, boto3, json
 from boto3.dynamodb.conditions import Key, Attr
 from flask_session import Session 
 
-accessKey = ""
-secretKey = ""
-sessToken = ""
+accessKey = "ASIA6AONFUCHMRWW54UP"
+secretKey = "TvDdoDLBH1vFTTvKxKvsYTh7GQ45ynY0B2VCFJoF"
+sessToken = "FwoGZXIvYXdzEIH//////////wEaDBIPCT0K4tMcgw8lUyLAAahTAxgQhjsy37aZ6drn48Gqag41u1YHfSuXG+o9JyUplBBil7rgj5wHxpFHjbroD+emi1JVwxwVWykvWI5+Je8CTxoXFvXqkcQ6AI7i8uyhc2g+rNsJl6rwKOI4mJjrDOO4yYHyjUvu5/CMk2OD6siRFuHDY2Ns+LL91PFJMm4sfv9QZ+5QVC2rNOUbw4cF1UL0C6GdBZgLpf+EXvPZO8tCcLrF6/lkqa8Q4nFB8bllSltG6lAmJPVEnqWncrpWrCjuk4yhBjIt/qCMBFYBdWrmCDLAGouaMXi0Cyl13KlDMJNSipAtixYsJrot4QjI9eBqwz/B"
 
 sessionBoto = boto3.session.Session(
     aws_access_key_id=accessKey,
@@ -25,28 +25,43 @@ Session(app)
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    
     try:
         if session["user"]:
             
             # Grab User's current list of tasks 
             table = dynamodb.Table('tasks')
-             
+            
+            # Access current User data using Lambda Function LambdaUserTasks
             payload = {"Email" :  session["user"]}
             response = lambdaConnection.invoke(FunctionName= "LambdaUsersTasks", InvocationType='RequestResponse', Payload=json.dumps(payload))
             answer = response["Payload"].read()
             answer = json.loads(answer)
             
             flash(answer)
+            
             if request.method == "POST":
+                email  = session["user"]
                 name = request.form.get('name')
-                flash(name)
-                return redirect("/")
-            return render_template("index.html")
+                description = request.form.get('description')
+                date = request.form.get("date")
+                
+                # Send form data to the Add Task Lambda function
+                payload = {"Email" : email, "Name" : name, "Description": description, "Date": date }
+                response = lambdaConnection.invoke(FunctionName= "AddTasks", InvocationType='RequestResponse', Payload=json.dumps(payload))
+                answer = response["Payload"].read()
+                answer = json.loads(answer)
+        
+                if(answer == "ALREADY EXISTS"):
+                    flash(answer)
+                    return render_template("index.html")
+                else:    
+                    session.pop('_flashes', None)
+                    return redirect("/")
         else:
-            return redirect("/login")
+            return redirect("/login")  
     except:
-        return redirect("/login")    
+        return redirect("/login")
+    return render_template("index.html")
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
